136f06639baf17d35bfe671dbfd7ebb720ff8dbf
hyunyoung <118007775+hyunyoung515@users.noreply.github.com>
2023-12-05 09:38:42 +0900

    수정

--- app/src/main/java/com/example/project/ArticleAdapter.kt
+++ app/src/main/java/com/example/project/ArticleAdapter.kt
@@ -21,11 +21,10 @@ class ArticleAdapter(val onItemClicked: (ArticleModel) -> Unit) : ListAdapter<Ar
 
             // Long 형식에서 날짜로 바꾸기.
             val format = SimpleDateFormat("MM월 dd일")
-            val date = Date(articleModel.createdAt) // Long -> Date
 
             binding.titleTextView.text = articleModel.title
-            binding.dateTextView.text = format.format(date).toString()
             binding.priceTextView.text = articleModel.price
+            binding.sellTextView.text = articleModel.sell
 
             // glide로 이미지 불러오기;
             if (articleModel.imageUrl.isNotEmpty()) {
@@ -55,11 +54,12 @@ class ArticleAdapter(val onItemClicked: (ArticleModel) -> Unit) : ListAdapter<Ar
         holder.bind(currentList[position])
     }
 
+
     companion object {
         val diffUtil = object : DiffUtil.ItemCallback<ArticleModel>() {
             override fun areItemsTheSame(oldItem: ArticleModel, newItem: ArticleModel): Boolean {
-                // 현재 노출하고 있는 아이템과 새로운 아이템이 같은지 비교;
-                return oldItem.createdAt == newItem.createdAt
+                // 현재 노출하고 있는 아이템과 새로운 아이템이 같은지 비교
+                return oldItem == newItem
             }
 
             override fun areContentsTheSame(oldItem: ArticleModel, newItem: ArticleModel): Boolean {
@@ -71,4 +71,3 @@ class ArticleAdapter(val onItemClicked: (ArticleModel) -> Unit) : ListAdapter<Ar
         }
     }
 }
-
